a75a775afca86f92f90aa22061ba21d24d1607e6
BRI-Systemkraft <150529751+BRI-Systemkraft@users.noreply.github.com>
2024-09-23 20:53:43 +0200

    Add files via upload

---
+++ install-ad.ps1
@@ -0,0 +1,18 @@
+﻿param (
+    [string]$DomainName,
+    [string]$NetBIOSName,
+    [string]$SafeModePassword
+)
+
+# Installiere die AD DS-Rolle
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Force
+
+# Erstelle den neuen AD-Domain-Controller
+Install-ADDSForest `
+    -DomainName $DomainName `
+    -DomainNetbiosName $NetBIOSName `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString $SafeModePassword -AsPlainText -Force) `
+    -Force
+
+# Starte den Server neu, um die Installation abzuschließen
+Restart-Computer
